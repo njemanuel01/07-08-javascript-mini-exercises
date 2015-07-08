979b9234f35ef7ed139317099aa7a244f35386a9
Nicholas Emanuel <njemanuel01@gmail.com>
2015-07-08 10:30:10 -0500

    Added html and functions to js

---
+++ tab-global.js
@@ -0,0 +1,11 @@
+//Declare variable
+var content = {ipsum: "oiwnefoiwndknowindfoiwndfkowodfn", lorum: "Lorem ipsum dolor sit amet, consectetur", dolor: "dddddddddddd"};
+
+//Gets content
+function find_content(input) {
+  return content.input;
+}
+
+function push_content(input) {
+  document.getElementById("content").innerText = find_content(input);
+} 
