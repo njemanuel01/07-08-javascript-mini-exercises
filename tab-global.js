eba7c3b96192b9295af10a03b791e7a6ad42253a
Nicholas Emanuel <njemanuel01@gmail.com>
2015-07-08 10:35:37 -0500

    Added function

--- tab-global.js
+++ tab-global.js
@@ -6,6 +6,13 @@ function find_content(input) {
   return content.input;
 }
 
+//Pushes content to div
 function push_content(input) {
   document.getElementById("content").innerText = find_content(input);
-} 
+}
+
+function tab_click() {
+  push_content(this.id)
+}
+
+ 
