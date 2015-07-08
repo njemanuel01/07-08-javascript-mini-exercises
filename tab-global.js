40d7717102446498ccec5496bc853016d396cbcc
Gwendolyn Holley <gmfholley@gmail.com>
2015-07-08 10:49:53 -0500

    Finished touches to our buttons and finished project

--- tab-global.js
+++ tab-global.js
@@ -3,16 +3,28 @@ var content = {ipsum: "oiwnefoiwndknowindfoiwndfkowodfn", lorum: "Lorem ipsum do
 
 //Gets content
 function find_content(input) {
-  return content.input;
+  return content[input];
 }
 
 //Pushes content to div
 function push_content(input) {
-  document.getElementById("content").innerText = find_content(input);
+  document.getElementById("content").innerHTML = find_content(input);
 }
 
-function tab_click() {
+
+//TODO - this could use a better name
+//displays content for this button in content div
+//
+// returns nothing
+function pull_tab_id_and_push_its_content() {
   push_content(this.id)
 }
 
- 
+
+window.onload = function() {
+  
+  document.getElementById("ipsum").addEventListener("click", pull_tab_id_and_push_its_content);
+  document.getElementById("lorum").addEventListener("click", pull_tab_id_and_push_its_content);
+  document.getElementById("dolor").addEventListener("click", pull_tab_id_and_push_its_content);
+  
+}
