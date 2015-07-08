260c077471fe7768b1f9725b16289e526a8de502
Gwendolyn Holley <gmfholley@gmail.com>
2015-07-08 13:33:42 -0500

    Starts javascript file
    Added paragraph and content divs to html
    Changed the name of content to paragraph
    Changed height of our container div to self-adjust

---
+++ accordion/accordion.js
@@ -0,0 +1,34 @@
+function change_to_visible(element) {
+  element.classList.add("visible");
+  element.classList.remove("invisible");
+}
+
+function change_to_invisible(element) {
+  element.classList.add("invisible");
+  element.classList.remove("visible");
+}
+
+
+
+function shows_this_paragraph() {
+  var bars = document.getElementsByClassName("bars");
+  for (i = 0; i < bars.length; i++ ){
+    change_to_visible(bars[i]);
+  }
+  
+  var paragraphs = document.getElementsByClassName("paragraph");
+  for (i = 0; i < paragraphs.length; i++ ){
+    change_to_invisible(paragraphs[i]);
+  }
+  
+  var cNodes = this.children;
+  //the zeroeth element is the bar
+  //the first element is the paragraph
+  change_to_invisible(cNodes[0]);
+  change_to_visible(cNodes[1]);
+  
+}
+
+window.onload {
+  document.getElementById("blog").addEventListener("click", shows_this_paragraph);
+}
