852a9f2f9a0c27615bc453245eb35ea6cdc0f9db
Nicholas Emanuel <njemanuel01@gmail.com>
2015-07-08 12:02:01 -0500

    Finished with transition

--- barcamp-example-build-absolute 2/index.js
+++ barcamp-example-build-absolute 2/index.js
@@ -7,11 +7,15 @@ function get_hamburger_menu() {
 function add_class_to_hamburger_menu() {
   var h = get_hamburger_menu();
   h.classList.add("grow_hamburger_menu");
+  h.style.height = "200px";
+  menu_added = true;
 }
 
 function remove_class_to_hamburger_menu() {
   var h = get_hamburger_menu();
   h.classList.remove("grow_hamburger_menu");
+  h.style.height = "0px";
+  menu_added = false;
 }
 
 function set_hamburger_menu() {
@@ -23,6 +27,6 @@ function set_hamburger_menu() {
 }
 
 window.onload = function() {
-  var x = document.getElementsByClass("menu");
+  var x = document.getElementsByClassName("menu");
   x[0].addEventListener("click", set_hamburger_menu); 
 }
