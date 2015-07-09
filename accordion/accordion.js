97f275716325f2c7ce2f15cee2165d488784a748
Gwendolyn Holley <gmfholley@gmail.com>
2015-07-09 10:25:37 -0500

    Finish refactoring accordion

--- accordion/accordion.js
+++ accordion/accordion.js
@@ -1,23 +1,15 @@
 var current_shown_section = "about";
 
-// This adds/removes a class off of an element, setting a certain div as visible
-// 
-// This accepts one argument, the element we wish to change
-// 
-// There is no return.
-function change_to_visible(element) {
-  element.classList.add("visible");
-  element.classList.remove("invisible");
+// this function will add a class if it isn't there and removes it if it is
+//
+// element is a DOM element
+//
+// returns nothing useful
+function toggle_visibility(element) {
+  element.classList.toggle("invisible");
+  element.classList.toggle("visible");
 }
-// This adds/removes a class off of an element, setting a certain div as invisible
-// 
-// This accepts one argument, the element we wish to change
-// 
-// There is no return.
-function change_to_invisible(element) {
-  element.classList.add("invisible");
-  element.classList.remove("visible");
-}
+
 // This calls a select method on each element of a class of our choice.
 // 
 // This accepts 2 arguments, the String of a class_name of the class we want to change, and
@@ -31,17 +23,20 @@ function make_uniform_elements(class_name, method_name_to_call) {
   }
 }
 
-function shows_this_paragraph() {
-  make_uniform_elements("bar", change_to_visible);
-  make_uniform_elements("paragraph", change_to_invisible);
-  //Step 1 - Use current_shown_section and get the associated element.
-  //Step 2 - Get element's children
-  //Step 3 - Do opposite of make_paragraph_visible
-  make_paragraph_visible(this);
-  //Step 4 - Set current_shown_section to be this.id
-  
+// using the current_shown_section variable, we hide the paragraph and show the bar
+//
+// returns nothing
+function reset_last_section () {
+  var this_element = document.getElementById(current_shown_section);
+  toggle_paragraph_and_bar(this_element);
 }
 
+// cycles through a class list array until it finds an element with the class_name_to_find
+//
+// array - array of Elements
+// class_name_to_find - String of the class we are trying to find among the array of Elements
+//
+// returns an Element matching the class_name
 function find_element_from_array(array, class_name_to_find) {
   for (i=0; i < array.length; i++) {
     if (array[i].className.indexOf(class_name_to_find) != -1) {
@@ -50,19 +45,30 @@ function find_element_from_array(array, class_name_to_find) {
   }
 }
 
-function make_paragraph_visible(element) {
+// for the element, finds the element's bar and paragraph children and toggles them visible/invisible
+//
+// element - DOM element
+//
+// returns nothing
+function toggle_paragraph_and_bar(element) {
   var cNodes = element.children;
-  //the zeroeth element is the bar
-  //the first element is the paragraph
-  change_to_visible(find_element_from_array(cNodes, "paragraph"));
-  change_to_invisible(find_element_from_array(cNodes, "bar"));
-  // change_to_visible(cNodes[1]);
+  toggle_visibility(find_element_from_array(cNodes, "paragraph"));
+  toggle_visibility(find_element_from_array(cNodes, "bar"));
 }
 
+// hides previous section and shows this clicked section
+//
+// returns nothing
+function shows_this_paragraph() {  
+  reset_last_section();
+  toggle_paragraph_and_bar(this);
+  current_shown_section = this.id;
+}
+
+
 window.onload = function() {
-  document.getElementById("blog").addEventListener("click", shows_this_paragraph);
-  document.getElementById("about").addEventListener("click", shows_this_paragraph);
-  document.getElementById("services").addEventListener("click", shows_this_paragraph);
-  document.getElementById("portfolio").addEventListener("click", shows_this_paragraph);
-  document.getElementById("contact").addEventListener("click", shows_this_paragraph);
+  var all = document.getElementsByClassName("section");
+  for (i = 0; i < all.length; i++) {
+    all[i].addEventListener("click", shows_this_paragraph);
+  }
 }
